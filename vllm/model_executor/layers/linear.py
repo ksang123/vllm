dca597f5a0a7ad228e34daf449fcc1fda7028d1d
ksang123 <etaizilberman@gmail.com>
2026-01-18 20:33:10 +0200

    commit

--- vllm/model_executor/layers/linear.py
+++ vllm/model_executor/layers/linear.py
@@ -1319,6 +1319,7 @@ class RowParallelLinear(LinearBase):
         *,
         return_bias: bool = True,
         disable_tp: bool = False,
+        opt: str = "",
     ):
         # Divide the weight matrix along the first dimension.
         self.tp_rank = get_tensor_model_parallel_rank() if not disable_tp else 0
@@ -1340,7 +1341,7 @@ class RowParallelLinear(LinearBase):
 
         self.input_is_parallel = input_is_parallel
         self.reduce_results = reduce_results
-
+        self.opt = opt
         assert self.quant_method is not None
         self.quant_method.create_weights(
             layer=self,
@@ -1435,7 +1436,7 @@ class RowParallelLinear(LinearBase):
         # Only fuse bias add into GEMM for rank 0 (this ensures that
         # bias will not get added more than once in TP>1 case)
         bias_ = None if (self.tp_rank > 0 or self.skip_bias_add) else self.bias
-        output_parallel = self.quant_method.apply(self, input_parallel, bias_, opt="down")
+        output_parallel = self.quant_method.apply(self, input_parallel, bias_, opt=self.opt)
 
         if self.reduce_results and self.tp_size > 1:
             output = tensor_model_parallel_all_reduce(output_parallel)
